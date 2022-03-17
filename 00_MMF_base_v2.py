882317b013c37d34912bc0494ad5173c6615e168
jessh123456 <83625253+jessh123456@users.noreply.github.com>
2022-03-18 12:17:33 +1300

    component 1

--- 00_MMF_base_v2.py
+++ 00_MMF_base_v2.py
@@ -1,6 +1,18 @@
+"""Added 01_name_not_blank_v3 to original v1 of this base code"""
+
 # Import statements
 
 # Functions go here
+
+# Check that ticket name is not blank
+def not_blank(question):
+    while True:
+        response = input(question)
+        if not response.isalpha():
+            print("You can't leave this blank...")
+        else:
+            return response
+
 
 # ******* Main routine *******
 
@@ -12,6 +24,7 @@
 # Loop to get ticket details
 
     # Get name (can't be blank)
+    name = not_blank("What's your name: ")
 
     # Get age (between 12-130)
 
